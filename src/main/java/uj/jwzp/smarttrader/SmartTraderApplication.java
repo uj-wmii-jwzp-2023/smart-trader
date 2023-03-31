c6ae192e4915de53bb3f39056b409444786e05d5
natdurlik <natalia.a.durlik@gmail.com>
2023-03-31 12:35:05 +0200

    Add YahooFinance library

--- src/main/java/uj/jwzp/smarttrader/SmartTraderApplication.java
+++ src/main/java/uj/jwzp/smarttrader/SmartTraderApplication.java
@@ -2,12 +2,30 @@ package uj.jwzp.smarttrader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class SmartTraderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartTraderApplication.class, args);
+    }
+
+    Stock stock;
+
+    {
+        try {
+            stock = YahooFinance.get("TSLA");
+            BigDecimal price = stock.getQuote(true).getPrice();
+            stock.print();
+            System.out.println("TSLA price: " + price);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
