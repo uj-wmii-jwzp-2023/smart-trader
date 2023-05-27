a1c6a96d606fa2c7ee81bd65b412016cc9b8d25d
mateuszpoleski <mateusz.poleski@student.uj.edu.pl>
2023-05-27 15:13:11 +0200

    Migrating for PostgreSQL to MongoDB database.

--- src/main/java/uj/jwzp/smarttrader/security/CustomUserDetailsService.java
+++ src/main/java/uj/jwzp/smarttrader/security/CustomUserDetailsService.java
@@ -26,7 +26,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByName(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        User user = userRepository.findUserByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
         return new org.springframework.security.core.userdetails.User(
                 user.getName(),
                 user.getPassword(),
@@ -34,6 +35,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     private Collection<GrantedAuthority> rolesToAuthorities(List<Role> roles) {
-        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.name())).collect(Collectors.toList());
     }
 }
